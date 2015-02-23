348268d3befb267a9d111d9d1ee538e6be0168e2
Tony Chen <tonylc@gmail.com>
2015-02-22 23:38:09 -0800

    mouseenter works

--- home.go
+++ home.go
@@ -5,15 +5,32 @@ import (
   "log"
   "net/http"
   "path"
+  "strings"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-  lp := path.Join("templates", "layout.tmpl")
-  var baseTemplate = r.URL.Path + ".tmpl"
-  if r.URL.Path == "/" {
-    baseTemplate = "redirect.tmpl"
+  if (strings.HasPrefix(r.URL.Path, "/templates")) {
+    staticHtmlHandler(w, r.URL.Path)
+  } else {
+    if r.URL.Path == "/" {
+      goTemplateHandler(w, "redirect.tmpl")
+    } else {
+      goTemplateHandler(w, r.URL.Path + ".tmpl")
+    }
   }
-  fp := path.Join("templates", baseTemplate)
+}
+
+// path is /templates/test.html
+func staticHtmlHandler(w http.ResponseWriter, basePath string) {
+  // strip leading '/'
+  fp := basePath[1:]
+  tmpl, _ := template.New("").Delims("<<", ">>").ParseFiles(fp)
+  tmpl.ExecuteTemplate(w, "body", nil)
+}
+
+func goTemplateHandler(w http.ResponseWriter, basePath string) {
+  lp := path.Join("templates", "layout.tmpl")
+  fp := path.Join("templates", basePath)
   tmpl, _ := template.New("").Delims("<<", ">>").ParseFiles(lp, fp)
   tmpl.ExecuteTemplate(w, "layout", nil)
 }
